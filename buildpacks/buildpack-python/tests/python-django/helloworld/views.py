0b50b07cd2d1f75a8267ec109885b5976e73038d
Deokhyun Ko <mainto@gmail.com>
2016-02-26 21:40:15 +0100

    remove unavailable buildpacks, update nodejs buildpack url

--- buildpacks/buildpack-python/tests/python-django/helloworld/views.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("python-django\n")
--- buildpacks/buildpack-ruby/tests/ruby-sinatra/web.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra'
-
-set :port, ENV["PORT"] || 5000
-
-get '/' do
-  "ruby-sinatra\n"
-end
